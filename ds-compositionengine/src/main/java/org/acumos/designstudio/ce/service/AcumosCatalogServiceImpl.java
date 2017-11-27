eda20891088d593cd47cacf18a6525576992c38e
Ashwin Sharma <as214g@att.com>
2017-11-26 23:56:37 -0600

    Removed release-notes.md from docs/
    Change-Id: I4ff4afeff2d6f044f8e8578f4a11a0a84d2e77ac
    Signed-off-by: Ashwin Sharma <as214g@att.com>

---
+++ ds-compositionengine/src/main/java/org/acumos/designstudio/ce/service/AcumosCatalogServiceImpl.java
@@ -0,0 +1,291 @@
+/*-
+ * ===============LICENSE_START=======================================================
+ * Acumos
+ * ===================================================================================
+ * Copyright (C) 2017 AT&T Intellectual Property & Tech Mahindra. All rights reserved.
+ * ===================================================================================
+ * This Acumos software file is distributed by AT&T and Tech Mahindra
+ * under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *  
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ * This file is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ===============LICENSE_END=========================================================
+ */
+
+package org.acumos.designstudio.ce.service;
+
+import java.io.ByteArrayOutputStream;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import org.acumos.cds.client.CommonDataServiceRestClientImpl;
+import org.acumos.cds.domain.MLPArtifact;
+import org.acumos.cds.domain.MLPSolutionRevision;
+import org.acumos.designstudio.ce.exceptionhandler.AcumosException;
+import org.acumos.designstudio.ce.exceptionhandler.ServiceException;
+import org.acumos.designstudio.ce.util.ConfigurationProperties;
+import org.acumos.designstudio.ce.util.EELFLoggerDelegator;
+import org.acumos.designstudio.ce.util.Properties;
+import org.acumos.nexus.client.NexusArtifactClient;
+import org.acumos.nexus.client.RepositoryLocation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * 
+ * 
+ *
+ */
+@Service
+public class AcumosCatalogServiceImpl implements IAcumosCatalog {
+	private static EELFLoggerDelegator logger = EELFLoggerDelegator.getLogger(AcumosCatalogServiceImpl.class);
+
+	@Autowired
+	ConfigurationProperties properties;
+
+	@Autowired
+	CommonDataServiceRestClientImpl cmnDataService;
+
+	@Autowired
+	Properties props;
+
+	@Autowired
+	NexusArtifactClient nexusArtifactClient;
+
+	
+	@Override
+	public String fetchJsonTOSCA(String solutionId, String version) {
+		logger.debug(EELFLoggerDelegator.debugLogger, "--------fetchJsonTOSCA()-------- : Begin");
+
+		String result = "";
+		String error = "{\"errorCode\" : \"%s\", \"errorDescription\" : \"%s\"}";
+
+		List<MLPSolutionRevision> mlpSolutionRevisionList;
+		String solutionRevisionId = null;
+		ByteArrayOutputStream byteArrayOutputStream = null;
+		List<MLPArtifact> mlpArtifactList;
+		try {
+			// 1. Get the list of SolutionRevision for the solutionId.
+			mlpSolutionRevisionList = getSolutionRevisionsList(solutionId);
+
+			// 2. Match the version with the SolutionRevision and get the solutionRevisionId.
+			if (null != mlpSolutionRevisionList && !mlpSolutionRevisionList.isEmpty()) {
+				solutionRevisionId = mlpSolutionRevisionList.stream().filter(mlp -> mlp.getVersion().equals(version))
+						.findFirst().get().getRevisionId();
+				logger.debug(EELFLoggerDelegator.debugLogger,
+						"------ SolutionRevisonId for Version : " + solutionRevisionId);
+			} else {
+				result = String.format(error, "501", "Failed to fetch the Solution Revision List");
+			}
+		} catch (Exception e) {
+			logger.error(EELFLoggerDelegator.errorLogger,
+					"---------Error : Exception in fetchJsonTOSCA() : Failed to fetch the Solution Revision List----------",
+					e);
+			result = String.format(error, "501",
+					"Failed to fetch the Solution Revision List for the version" + version);
+		}
+
+		if (null != solutionRevisionId) {
+			// 3. Get the list of Artifiact for the SolutionId and SolutionRevisionId.
+			mlpArtifactList = getListOfArtifacts(solutionId, solutionRevisionId);
+
+			String artifactType = props.getArtifactType();
+			String nexusURI = "";
+			if (null != mlpArtifactList && !mlpArtifactList.isEmpty()) {
+				try {
+					nexusURI = mlpArtifactList.stream()
+							.filter(mlpArt -> mlpArt.getArtifactTypeCode().equalsIgnoreCase(artifactType)).findFirst()
+							.get().getUri();
+					logger.debug(EELFLoggerDelegator.debugLogger, "------ Nexus URI : " + nexusURI);
+
+					if (null != nexusURI) {
+						byteArrayOutputStream = getPayload(nexusURI);
+						logger.debug(EELFLoggerDelegator.debugLogger,
+								"------ Response in String Format : " + byteArrayOutputStream.toString() + " -------");
+						result = byteArrayOutputStream.toString();
+					} else {
+						result = String.format(error, "504",
+								"Could not search the artifact URI for artifactType " + artifactType);
+					}
+				} catch (Exception e) {
+					logger.error(EELFLoggerDelegator.errorLogger,
+							"---------Error : Exception in fetchJsonTOSCA() : Failed to fetch the Solution Revision List----------",
+							e);
+					result = String.format(error, "504",
+							"Could not search the artifact URI for artifactType TG" + artifactType);
+
+				}
+
+			} else {
+				result = String.format(error, "503", "Failed to fetch the Artifact Details for " + solutionId
+						+ "with solutionRevisionId" + solutionRevisionId);
+			}
+		} else {
+			result = String.format(error, "502",
+					"Failed to fetch the solutionRevisionId for " + solutionId + " with version " + version);
+		}
+		logger.debug(EELFLoggerDelegator.debugLogger, "--------fetchJsonTOSCA()-------- : End");
+		return result;
+	}
+
+
+	@Override
+	public String saveCompositeSolution() {
+		String result = "";
+
+		return result;
+
+	}
+
+	/**
+	 * 
+	 * @param solutionId
+	 * @return
+	 */
+	private List<MLPSolutionRevision> getSolutionRevisionsList(String solutionId) {
+		logger.debug(EELFLoggerDelegator.debugLogger, "------- getSolutionRevisions() : Begin ----------");
+		List<MLPSolutionRevision> solRevisionsList = null;
+		try {
+			solRevisionsList = cmnDataService.getSolutionRevisions(solutionId);
+		} catch (Exception e) {
+			logger.error(EELFLoggerDelegator.errorLogger, "-------- Exception in getSolutionRevisions() ----------",e);
+		}
+		logger.debug(EELFLoggerDelegator.debugLogger, "------- getSolutionRevisions() : End ----------");
+		return solRevisionsList;
+	}
+	/**
+	 * 
+	 * @param solutionId
+	 * @param solutionRevisionId
+	 * @return
+	 */
+	private List<MLPArtifact> getListOfArtifacts(String solutionId, String solutionRevisionId) {
+		List<MLPArtifact> mlpArtifactsList = null;
+		try {
+			mlpArtifactsList = cmnDataService.getSolutionRevisionArtifacts(solutionId, solutionRevisionId);
+		} catch (Exception e) {
+			logger.error(EELFLoggerDelegator.errorLogger, "-------- Exception in getListOfArtifacts() ----------",e);
+		}
+		return mlpArtifactsList;
+
+	}
+	/**
+	 * 
+	 * @param uri
+	 * @return
+	 */
+	private ByteArrayOutputStream getPayload(String uri) {
+
+		ByteArrayOutputStream outputStream = null;
+		try {
+
+			outputStream = nexusArtifactClient.getArtifact(uri);
+			if (outputStream != null) {
+				outputStream.close();
+			}
+
+		} catch (Exception ex) {
+			logger.error(EELFLoggerDelegator.errorLogger, "-------- Exception in getListOfArtifacts() ----------",ex);
+			
+		}
+
+		return outputStream;
+
+	}
+
+	
+	@Override
+	public String readArtifact(String userId, String solutionId, String version, String artifactType)
+			throws AcumosException {
+		logger.debug(EELFLoggerDelegator.debugLogger, "--------readArtifact()-------- : Begin");
+
+		String result = "";
+
+		List<MLPSolutionRevision> mlpSolutionRevisionList;
+		String solutionRevisionId = null;
+		ByteArrayOutputStream byteArrayOutputStream = null;
+		List<MLPArtifact> mlpArtifactList;
+		try {
+			// 1. Get the list of SolutionRevision for the solutionId.
+			mlpSolutionRevisionList = getSolutionRevisionsList(solutionId);
+
+			// 2. Match the version with the SolutionRevision and get the solutionRevisionId.
+			if (null != mlpSolutionRevisionList && !mlpSolutionRevisionList.isEmpty()) {
+				solutionRevisionId = mlpSolutionRevisionList.stream()
+						.filter(mlp -> mlp.getVersion().equals(version) && mlp.getOwnerId().equalsIgnoreCase(userId))
+						.findFirst().get().getRevisionId();
+				logger.debug(EELFLoggerDelegator.debugLogger,
+						"------ SolutionRevisonId for Version : " + solutionRevisionId + " -------");
+			}
+		} catch (NoSuchElementException | NullPointerException e) {
+			logger.error(EELFLoggerDelegator.errorLogger,
+					"---------Error : Exception in readArtifact() : Failed to fetch the Solution Revision Id----------",
+					e);
+			throw new NoSuchElementException("Failed to fetch the Solution Revision Id of the solutionId for the user");
+		} catch (Exception e) {
+			logger.error(EELFLoggerDelegator.errorLogger,
+					"---------Error : Exception in readArtifact() : Failed to fetch the Solution Revision Id----------",
+					e);
+			throw new ServiceException("Failed to fetch the Solution Revision Id for the solutionId " + solutionId);
+		}
+
+		if (null != solutionRevisionId) {
+			// 3. Get the list of Artifiact for the SolutionId and SolutionRevisionId.
+			mlpArtifactList = getListOfArtifacts(solutionId, solutionRevisionId);
+			String nexusURI = "";
+			if (null != mlpArtifactList && !mlpArtifactList.isEmpty()) {
+				try {
+					nexusURI = mlpArtifactList.stream()
+							.filter(mlpArt -> mlpArt.getArtifactTypeCode().equalsIgnoreCase(artifactType)).findFirst()
+							.get().getUri();
+					logger.debug(EELFLoggerDelegator.debugLogger, "------ Nexus URI : " + nexusURI + " -------");
+
+					if (null != nexusURI) {
+						byteArrayOutputStream = getPayload(nexusURI);
+						logger.debug(EELFLoggerDelegator.debugLogger,
+								"------ Response in String Format : " + byteArrayOutputStream.toString() + " -------");
+						result = byteArrayOutputStream.toString();
+					}
+				} catch (NoSuchElementException | NullPointerException e) {
+					logger.error(EELFLoggerDelegator.errorLogger,
+							"---------Error : Exception in readArtifact() : Failed to fetch the artifact URI for artifactType----------",
+							e);
+					throw new NoSuchElementException(
+							"Could not search the artifact URI for artifactType " + artifactType);
+				} catch (Exception e) {
+					logger.error(EELFLoggerDelegator.errorLogger,
+							"---------Error : Exception in readArtifact() : Failed to fetch the artifact URI for artifactType----------",
+							e);
+					throw new ServiceException(
+							" --------------- Exception Occured decryptAndWriteTofile() --------------", "501",
+							"Could not search the artifact URI for artifactType " + artifactType, e.getCause());
+				}
+			}
+		}
+		logger.debug(EELFLoggerDelegator.debugLogger, "--------readArtifact()-------- : End");
+		return result;
+	}
+	/**
+	 * 
+	 * @param url
+	 * @param user
+	 * @param pass
+	 */
+	public void getRestClient(String url,String user, String pass){
+		cmnDataService = new CommonDataServiceRestClientImpl(url, user, pass);
+	}
+	/**
+	 * 
+	 * @param repositoryLocation
+	 */
+	public void getNexusClient(RepositoryLocation repositoryLocation){
+		nexusArtifactClient = new NexusArtifactClient(repositoryLocation);
+    }
+
+}
---
+++ ds-compositionengine/src/test/java/org/acumos/designstudio/test/VOTest.java
@@ -0,0 +1,345 @@
+/*-
+ * ===============LICENSE_START=======================================================
+ * Acumos
+ * ===================================================================================
+ * Copyright (C) 2017 AT&T Intellectual Property & Tech Mahindra. All rights reserved.
+ * ===================================================================================
+ * This Acumos software file is distributed by AT&T and Tech Mahindra
+ * under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *  
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ * This file is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ===============LICENSE_END=========================================================
+ */
+
+/**
+ * 
+ */
+package org.acumos.designstudio.test;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import org.acumos.cds.client.CommonDataServiceRestClientImpl;
+import org.acumos.designstudio.cdump.Capability;
+import org.acumos.designstudio.ce.exceptionhandler.DAOException;
+import org.acumos.designstudio.ce.util.ConfigurationProperties;
+import org.acumos.designstudio.ce.util.EELFLoggerDelegator;
+import org.acumos.designstudio.ce.vo.DSSolution;
+import org.acumos.designstudio.ce.vo.blueprint.BluePrint;
+import org.acumos.designstudio.ce.vo.blueprint.Depends_On;
+import org.acumos.designstudio.ce.vo.blueprint.InputOperationSignatures;
+import org.acumos.designstudio.ce.vo.blueprint.Node;
+import org.acumos.designstudio.ce.vo.blueprint.OperationSignature;
+import org.acumos.designstudio.ce.vo.tgif.Artifact;
+import org.acumos.designstudio.ce.vo.tgif.Auxiliary;
+import org.acumos.designstudio.ce.vo.tgif.Call;
+import org.acumos.designstudio.ce.vo.tgif.Provide;
+import org.acumos.designstudio.ce.vo.tgif.Request;
+import org.acumos.designstudio.ce.vo.tgif.Response;
+import org.json.simple.JSONArray;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+
+
+/**
+ * 
+ *
+ */
+public class VOTest  {
+	private static final EELFLoggerDelegator logger = EELFLoggerDelegator.getLogger(VOTest.class);
+	private String nexusURI = "";
+	private String payloadURI ="";
+	public static Properties CONFIG = new Properties();
+	@Before
+	/**
+	 * 
+	 * @throws Exception
+	 */
+	public void createClient() throws Exception {
+	    CONFIG.load(GetMatchingModelTest.class.getResourceAsStream("/application.properties"));
+	    payloadURI = CONFIG.getProperty("nexus.nexusendpointurlTest");
+	    nexusURI = CONFIG.getProperty("nexus.nexusUriTestt");
+	}
+	
+	@Test
+	public void testTgifArtifact(){
+		Artifact arti = new Artifact();
+		arti.setType("docker image");
+		arti.setUri("fake.nexus.org/dcae/kpi_anomaly:1.0.0");
+		arti.getType();
+		arti.getUri();
+		assertNotNull(arti);
+		assertEquals("docker image", arti.getType());
+		assertEquals("fake.nexus.org/dcae/kpi_anomaly:1.0.0", arti.getUri());
+		logger.debug(EELFLoggerDelegator.debugLogger, arti.toString());
+	}
+	
+	@Test
+	public void testVOArtifact(){
+		org.acumos.designstudio.ce.vo.Artifact artifact = new org.acumos.designstudio.ce.vo.Artifact();
+		artifact.setContentLength(100);
+		artifact.setExtension(".any");
+		artifact.setName("Nexus Repo");
+		artifact.setNexusURI(nexusURI);
+		artifact.setPayloadURI(payloadURI);
+		artifact.setSolutionID("0016c81e-6fe7-4d93-9763-a68c76c4ce8a");
+		artifact.setType("TOSCAPROTOBUF");
+		artifact.setVersion("1.0.0");
+		artifact.getContentLength();
+		artifact.getExtension();
+		artifact.getName();
+		artifact.getNexusURI();
+		artifact.getPayloadURI();
+		artifact.getSolutionID();
+		artifact.getType();
+		artifact.getVersion();
+		assertNotNull(artifact);
+		assertEquals(".any", artifact.getExtension());
+		assertEquals("Nexus Repo", artifact.getName());
+		assertEquals("TOSCAPROTOBUF", artifact.getType());
+		assertEquals("1.0.0", artifact.getVersion());
+		logger.debug(EELFLoggerDelegator.debugLogger, artifact.toString());
+		
+	}
+	
+	@Test
+	public void testTgifAuxiliary(){
+		Auxiliary aux = new Auxiliary();
+		aux.setTemp("Temp");
+		aux.getTemp();
+		assertNotNull(aux);
+		assertEquals("Temp", aux.getTemp());
+		logger.debug(EELFLoggerDelegator.debugLogger, aux.toString());
+	}
+	
+	@Test
+	public void testVOBluePrint(){
+		BluePrint bp = new BluePrint();
+		
+		bp.setName("BluePrint");
+		bp.getName();
+		bp.setVersion("1.0.0");
+		bp.getVersion();
+		
+		Node n = new Node();
+		List<Node> nodeList = new ArrayList<>();
+		
+		n.setContainer_name("Blueprint Orchestrator");
+		n.getContainer_name();
+		n.setImage("BPDI");
+		n.getImage();
+		
+		nodeList.add(n);
+		
+		bp.setNodes(nodeList);
+		bp.getNodes();
+		
+		Depends_On don = new Depends_On();
+		don.setName("BluePrint");
+		don.getName();
+		
+		OperationSignature osig = new OperationSignature();
+		osig.setOperation("transform");
+		osig.getOperation();
+		
+		don.setOperation_signature(osig);
+		don.getOperation_signature();
+		
+		
+		List<Depends_On> donList = new ArrayList<>();
+		donList.add(don);
+		
+		n.setDepends_on(donList);
+		n.getDepends_on();
+		
+		InputOperationSignatures ios = new InputOperationSignatures();
+		ios.setOperation("Auxialiary");
+		ios.getOperation();
+		
+		List<InputOperationSignatures> iosList = new ArrayList<>();
+		iosList.add(ios);
+		
+		bp.setInput_operation_signatures(iosList);
+		bp.getInput_operation_signatures();
+		logger.debug(EELFLoggerDelegator.debugLogger, bp.toString());
+		
+		assertNotNull(bp);
+		assertEquals("BluePrint", bp.getName());
+
+		assertNotNull(n);
+		assertEquals("BPDI", n.getImage());
+
+		assertNotNull(don);
+		assertEquals("BluePrint", don.getName());
+	}
+	
+
+	@Test
+	public void testTgifCall(){
+		Call call = new Call();
+		call.setConfig_key("transform");
+		call.getConfig_key();
+		
+		Request req = new Request();
+		JSONArray reqFormat = new JSONArray();
+		
+		req.setFormat(reqFormat);
+		req.getFormat();
+		
+		call.setRequest(req);
+		call.getRequest();
+		
+		Response res = new Response();
+		
+		JSONArray formt = new JSONArray();
+		res.setFormat(formt);
+		res.getFormat();
+		res.setVersion("1.0.0");
+		res.getVersion();
+		
+		call.setResponse(res);
+		call.getResponse();
+		assertNotNull(call);
+		assertEquals("transform", call.getConfig_key());
+
+		assertNotNull(res);
+		assertEquals("1.0.0", res.getVersion());
+		logger.debug(EELFLoggerDelegator.debugLogger, call.toString());
+	}
+	
+	@Test
+	public void testTgifParameter(){
+		String s1 = "module.arg.model";
+		String s2 = "TGIF";
+		String s3 = "data/102915pySGDonelinec2_0_ffq_2.mod";
+		org.acumos.designstudio.ce.vo.tgif.Parameter param = new org.acumos.designstudio.ce.vo.tgif.Parameter();
+		param.setDescription(s1);
+		param.setName(s2);
+		param.setValue(s3);
+		Assert.assertEquals(s1, param.getDescription());
+		Assert.assertEquals(s2, param.getName());
+		Assert.assertEquals(s3, param.getValue());
+		logger.debug(EELFLoggerDelegator.debugLogger, "---- " + param.toString());
+	}
+	
+	@Test
+	public void testTgifProvide(){
+		Provide provide = new Provide();
+		
+		Request re = new Request();
+		JSONArray reqFormat = new JSONArray();
+		re.setFormat(reqFormat);
+		re.getFormat();
+		provide.setRequest(re);
+		
+		Response res = new Response();
+		JSONArray formt = new JSONArray();
+		res.setFormat(formt);
+		res.getFormat();
+		res.setVersion("1.0.0");
+		res.getVersion();
+		provide.setResponse(res);
+		provide.setRoute("IGNORE");
+		assertNotNull(re);
+
+		assertNotNull(res);
+		assertEquals("1.0.0", res.getVersion());
+
+		assertNotNull(provide);
+		assertEquals("IGNORE", provide.getRoute());
+		logger.debug(EELFLoggerDelegator.debugLogger, "---- " + provide.toString());
+		
+	}
+	
+	@Test
+	public void testCdumpCapability(){
+		String s1 = "123456";
+		String s2 = "ReqCapability";
+		Capability cap = new Capability();
+		cap.setId(s1);
+		cap.setName(s2);
+		Assert.assertEquals(s1, cap.getId());
+		Assert.assertEquals(s2, cap.getName());
+		logger.debug(EELFLoggerDelegator.debugLogger, "---- " + cap,toString());
+	}
+	
+	@Test
+	public void testDSSolution(){
+		String s1 = "1";
+		String s2 = "1";
+		String s3 = "1";
+		String s4 = "1";
+		String s5 = "1";
+		String s6 = "1";
+		String s7 = "1";
+		String s8 = "1";
+		String s9 = "1";
+		String s10 = "1";
+		String s11 = "1";
+		String s12 = "1";
+		String s13 = "1";
+		String s14 = "1";
+		DSSolution ds = new DSSolution(s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11, s12, s13, s14);
+		logger.debug(EELFLoggerDelegator.debugLogger, "---- " + ds.toString());
+		DSSolution dss = new DSSolution();
+		dss.setAuthor(s1);
+		Assert.assertEquals(s1, dss.getAuthor());
+		dss.setCategory(s2);
+		Assert.assertEquals(s2, dss.getCategory());
+		dss.setCreatedDate(s3);
+		Assert.assertEquals(s3, dss.getCreatedDate());
+		dss.setDescription(s4);
+		Assert.assertEquals(s4, dss.getDescription());
+		dss.setIcon(s5);
+		Assert.assertEquals(s5,dss.getIcon());
+		dss.setModifiedDate(s6);
+		Assert.assertEquals(s6,dss.getModifiedDate());
+		dss.setOnBoarder(s7);
+		Assert.assertEquals(s7,dss.getOnBoarder());
+		dss.setProvider(s8);
+		Assert.assertEquals(s8,dss.getProvider());
+		dss.setSolutionId(s9);
+		Assert.assertEquals(s9,dss.getSolutionId());
+		dss.setSolutionName(s10);
+		Assert.assertEquals(s10,dss.getSolutionName());
+		dss.setSolutionRevisionId(s11);
+		Assert.assertEquals(s11,dss.getSolutionRevisionId());
+		dss.setToolKit(s12);
+		Assert.assertEquals(s12,dss.getToolKit());
+		dss.setVersion(s13);
+		Assert.assertEquals(s13,dss.getVersion());
+		dss.setVisibilityLevel(s14);
+		Assert.assertEquals(s14,dss.getVisibilityLevel());
+		logger.debug(EELFLoggerDelegator.debugLogger, "---- " + dss.toString());
+	}
+	
+	@Test
+	public void testUtilConfigurationProperties(){
+		ConfigurationProperties confProps = new ConfigurationProperties();
+		confProps.getCmndatasvcendpoinurl();
+		confProps.getCmndatasvcpwd();
+		confProps.getCmndatasvcuser();
+		confProps.getDateFormat();
+		confProps.getNexusendpointurl();
+		confProps.getNexusgroupid();
+		confProps.getNexuspassword();
+		confProps.getNexususername();
+		confProps.getToscaOutputFolder();
+		assertNotNull(confProps);
+		logger.debug(EELFLoggerDelegator.debugLogger, "---- " + confProps.toString());
+	}
+	
+
+}
