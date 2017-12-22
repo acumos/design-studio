c34cf6b40b9353ecd6cbbb945bb8718931ac5216
Neha Ganesh Bhoyar <NehaGanesh.Bhoyar@TechMahindra.com>
2017-12-22 21:00:46 +0530

    checkingin testcases for TOSCAGeneratorClient
    Change-Id: I545dd496e74e88d3c765a48eb8fd99f87d56ac2b
    Signed-off-by: Neha Ganesh Bhoyar <NehaGanesh.Bhoyar@TechMahindra.com>

--- TOSCAGeneratorClient/src/test/java/org/acumos/designstudio/toscagenerator/test/TgifGeneratorServiceTest.java
+++ TOSCAGeneratorClient/src/test/java/org/acumos/designstudio/toscagenerator/test/TgifGeneratorServiceTest.java
@@ -20,9 +20,14 @@
 
 package org.acumos.designstudio.toscagenerator.test;
 
-import org.acumos.designstudio.toscagenerator.ToscaGeneratorClient;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import org.acumos.designstudio.toscagenerator.exceptionhandler.AcumosException;
 import org.acumos.designstudio.toscagenerator.service.TgifGeneratorService;
-import org.junit.Assert;
+import org.acumos.designstudio.toscagenerator.vo.Artifact;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -30,25 +35,22 @@ import org.slf4j.LoggerFactory;
 public class TgifGeneratorServiceTest {
 	private static final Logger logger = LoggerFactory.getLogger(TgifGeneratorServiceTest.class);
 	
-	String localpath = System.getProperty("user.dir");
-	
-	
 	@Test
-	public void testCountTransport()throws Exception {
-		String metaData = "{\"schema\": \"acumos.schema.model:0.3.0\", \"runtime\": {\"name\": \"python\", \"encoding\": \"protobuf\", \"version\": \"3.6.1\", \"dependencies\": {\"pip\": {\"indexes\": [], \"requirements\": [{\"name\": \"cloudpickle\", \"version\": \"0.2.2\"}, {\"name\": \"numpy\", \"version\": \"1.12.1\"}, {\"name\": \"scikit-learn\", \"version\": \"0.18.1\"}, {\"name\": \"acumos-client\", \"version\": \"0.3.9\"}]}, \"conda\": {\"channels\": [], \"requirements\": []}}}, \"name\": \"Aggregator\", \"methods\": {\"transform\": {\"input\": \"DataFrame\", \"output\": \"Prediction\", \"description\": \"Transforms a DataFrame to a Prediction\"}}}";
+    public void testCountTransport1() {
+        String metaData = "{\"schema\": \"acumos.schema.model:0.3.0\", \"runtime\": {\"name\": \"python\", \"encoding\": \"protobuf\", \"version\": \"3.6.1\", \"dependencies\": {\"pip\": {\"indexes\": [], \"requirements\": [{\"name\": \"cloudpickle\", \"version\": \"0.2.2\"}, {\"name\": \"numpy\", \"version\": \"1.12.1\"}, {\"name\": \"scikit-learn\", \"version\": \"0.18.1\"}, {\"name\": \"acumos-client\", \"version\": \"0.3.9\"}]}, \"conda\": {\"channels\": [], \"requirements\": []}}}, \"name\": \"Aggregator\", \"methods\": {\"transform\": {\"input\": \"DataFrame\", \"output\": \"Prediction\", \"description\": \"Transforms a DataFrame to a Prediction\"}}}";
 		String protobuf1 = "{\"syntax\":\"proto3\",\"packageName\":\"\",\"listOfMessages\":[{\"messageName\":\"DataFrame\",\"messageargumentList\":[{\"firstToken\":\"repeated\",\"type\":\"int64\",\"name\":\"day\",\"tag\":\"1\"},{\"firstToken\":\"repeated\",\"type\":\"int64\",\"name\":\"weekday\",\"tag\":\"2\"},{\"firstToken\":\"repeated\",\"type\":\"int64\",\"name\":\"hour\",\"tag\":\"3\"},{\"firstToken\":\"repeated\",\"type\":\"int64\",\"name\":\"minute\",\"tag\":\"4\"},{\"firstToken\":\"repeated\",\"type\":\"double\",\"name\":\"hist_1D\",\"tag\":\"5\"},{\"firstToken\":\"repeated\",\"type\":\"int64\",\"name\":\"VM_ID\",\"tag\":\"6\"}]},{\"messageName\":\"Prediction\",\"messageargumentList\":[{\"firstToken\":\"repeated\",\"type\":\"double\",\"name\":\"predictions\",\"tag\":\"1\"}]}],\"service\":{\"name\":\"Model \",\"listOfOperations\":[{\"operationType\":\"rpc\",\"operatioName\":\"transform\",\"listOfInputMessages\":[{\"inputMessageName\":\"DataFrame\"}],\"listOfOutputMessages\":[{\"outPutMessageName\":\"Prediction\"}]}]}}";
-		try {
-			logger.info("Tgif file creation is  started");
-			ToscaGeneratorClient client = new ToscaGeneratorClient(" {\"toscaOutputFolder\":\" "+ localpath +"\", \"toscaGeneratorEndPointURL\":\"\", \"nexusEndPointURL\":\"\", \"nexusUserName\":\"\", \"nexusPassword\":\"\", \"nexusGroupId\":\"\", \"cmnDataSvcEndPoinURL\":\"\", \"cmnDataSvcUser\":\"\", \"cmnDataSvcPwd\":\"\"}");
-			TgifGeneratorService tgifGeneratorService = new TgifGeneratorService();
-			tgifGeneratorService.createTgif("3842ba62-8a0c-40cf-817a-ae9986e78aaf", "1.0.0", protobuf1, metaData);
-			logger.info("Tgif file is created");
-		} catch (Exception ex) {
-			logger.error("Failed", ex);
-			throw ex;
+		String tgifjsonString ="{\"self\":{\"version\":\"1.1.1\",\"name\":\"Gen\",\"description\":\"\",\"component_type\":\"Docker\"},\"streams\":{},\"services\":{\"calls\":[{\"config_key\":\"transform\",\"request\":{\"format\":[{\"messageName\":\"Prediction\",\"messageargumentList\":[{\"name\":\"myRow\",\"rule\":\"repeated\",\"tag\":\"1\",\"type\":\"int64\"}]}],\"version\":\"\"},\"response\":{\"format\":[],\"version\":\"\"}}],\"provides\":[{\"route\":\"transform\",\"request\":{\"format\":[{\"messageName\":\"DataFrame\",\"messageargumentList\":[{\"complexType\":{\"messageName\":\"DataFrameRow\",\"messageargumentList\":[{\"name\":\"sepal_len\",\"rule\":\"\",\"tag\":\"1.1\",\"type\":\"string\"},{\"name\":\"sepal_wid\",\"rule\":\"\",\"tag\":\"1.2\",\"type\":\"int32\"},{\"complexType\":{\"messageName\":\"SubFrameRow\",\"messageargumentList\":[{\"name\":\"row_1\",\"rule\":\"repeated\",\"tag\":\"1.3.1\",\"type\":\"string\"},{\"name\":\"row_2\",\"rule\":\"repeated\",\"tag\":\"1.3.2\",\"type\":\"string\"}]},\"name\":\"petal_len\",\"rule\":\"\",\"tag\":\"1.3\",\"type\":\"SubFrameRow\"},{\"name\":\"petal_wid\",\"rule\":\"\",\"tag\":\"1.4\",\"type\":\"string\"}]},\"name\":\"rows\",\"rule\":\"repeated\",\"tag\":\"1\",\"type\":\"DataFrameRow\"},{\"complexType\":{\"messageName\":\"MyFrameRow\",\"messageargumentList\":[{\"name\":\"row_1\",\"rule\":\"repeated\",\"tag\":\"2.1\",\"type\":\"int64\"},{\"name\":\"row_2\",\"rule\":\"repeated\",\"tag\":\"2.2\",\"type\":\"string\"}]},\"name\":\"myRow\",\"rule\":\"repeated\",\"tag\":\"2\",\"type\":\"MyFrameRow\"}]}],\"version\":\"\"},\"response\":{\"format\":[],\"version\":\"\"}}]},\"parameters\":[],\"auxiliary\":{},\"artifacts\":[]}";
+        Artifact result =  new Artifact("TGIF", "json",  "3842ba62", "1.1.1", "", tgifjsonString.length());
+		TgifGeneratorService mockito = mock(TgifGeneratorService.class);
+        try {
+			when(mockito.createTgif("KDFOAIDFJ@#IJDFLKS", "1.1.1", protobuf1, metaData)).thenReturn(result);
+			assertThat(result.getContentLength(), is(1371));
+		} catch (AcumosException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-	
-	}
+
+     }
 
 
 }
--- TOSCAGeneratorClient/src/test/java/org/acumos/designstudio/toscagenerator/test/ToscaGeneratorServiceTest.java
+++ TOSCAGeneratorClient/src/test/java/org/acumos/designstudio/toscagenerator/test/ToscaGeneratorServiceTest.java
@@ -20,86 +20,47 @@
 
 package org.acumos.designstudio.toscagenerator.test;
 
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 import java.io.File;
 import java.util.Properties;
 
-import org.acumos.cds.client.CommonDataServiceRestClientImpl;
 import org.acumos.cds.client.ICommonDataServiceRestClient;
 import org.acumos.designstudio.toscagenerator.ToscaGeneratorClient;
-import org.acumos.nexus.client.NexusArtifactClient;
-import org.acumos.nexus.client.RepositoryLocation;
-import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ToscaGeneratorServiceTest {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(ToscaGeneratorServiceTest.class);
-	
+
 	private String metaDataFile = "./metadata.json";
 	private String protoDataFile = "./aggregator-proto.proto";
-	private String userId = "c4e4d366-8ed8-40e1-b61e-1e103de9699b"; 
+	private String userId = "c4e4d366-8ed8-40e1-b61e-1e103de9699b";
 	private String solutionID = "3842ba62-8a0c-40cf-817a-ae9986e78aaf";
 	private String version = "1.0.0";
 	private String solutionRevisionID = "be647000-6ccf-4ec7-bf25-af538bc481ef";
-	private String toscaOutputFolder = System.getProperty("user.dir");
 	private String result = null;
-	private String url = "";
-	private String user ="";
-	private String pass = "";
-	private String toscaGeneratorEndPointURL = "";
-	private String nexusEndPointURL = "";
-	private String nexusUserName = "";
-	private String nexusPassword = "";
-	private String nexusGroupId="";
-	private NexusArtifactClient nexusArtifactClient = null;
-	private ICommonDataServiceRestClient cmnDataService;
+
 	public static Properties CONFIG = new Properties();
-	
-	@Before
-	public void createClient() throws Exception {
-	    CONFIG.load(ToscaGeneratorServiceTest.class.getResourceAsStream("/TOSCAApplication.properties"));
-		url = CONFIG.getProperty("cmndatasvc.cmndatasvcendpoinurlTest");
-		user = CONFIG.getProperty("cmndatasvc.cmndatasvcuserTest");
-		pass = CONFIG.getProperty("cmndatasvc.cmndatasvcpwdTest");
-		
-		nexusEndPointURL = CONFIG.getProperty("nexus.nexusendpointurlTest");
-		nexusUserName = CONFIG.getProperty("nexus.nexususernameTest");
-		nexusPassword = CONFIG.getProperty("nexus.nexuspasswordTest");
-		nexusGroupId = CONFIG.getProperty("nexus.nexusgroupidTest");
-		
-		toscaGeneratorEndPointURL = CONFIG.getProperty("toscaGeneratorEndPointURL");
-		
-		nexusArtifactClient = getNexusClient();
-		
-		cmnDataService = CommonDataServiceRestClientImpl.getInstance(url.toString(), user, pass);
-	}
-	private NexusArtifactClient getNexusClient() {
-		try {
-			RepositoryLocation repositoryLocation = new RepositoryLocation();
-			repositoryLocation.setId("1");
-			repositoryLocation.setUrl(CONFIG.getProperty("nexus.nexusendpointurlTest"));
-			repositoryLocation.setUsername(CONFIG.getProperty("nexus.nexususernameTest"));
-			repositoryLocation.setPassword(CONFIG.getProperty("nexus.nexuspasswordTest"));
-			nexusArtifactClient = new NexusArtifactClient(repositoryLocation);
-		} catch (Exception e) {
-			logger.error("Exception in getNexusClient()",e);
-			throw e;
-		}
-		return nexusArtifactClient;
-	}
-	
+
+	@Mock
+	private ICommonDataServiceRestClient cmnDataService;
+
+
 	@Test
-	public void testCountTransport() throws Exception{
-		ToscaGeneratorClient client = new ToscaGeneratorClient(toscaOutputFolder,  toscaGeneratorEndPointURL,  nexusEndPointURL,
-				 nexusUserName,  nexusPassword,  nexusGroupId,  url,
-				 user,  pass);
-		try{
+	public void testCountTransport() throws Exception {
+		
+		try {
+			ToscaGeneratorClient mockito = mock(ToscaGeneratorClient.class);
 			logger.info("Toscagenerator client start");
 			File localMetaDataFile = new File(metaDataFile);
 			File localProtoFile = new File(protoDataFile);
-			result = client.generateTOSCA(userId, solutionID, version,solutionRevisionID,localProtoFile, localMetaDataFile);
+			when(mockito.generateTOSCA(userId, solutionID, version, solutionRevisionID, localProtoFile,
+					localMetaDataFile)).thenReturn("Success");
 			logger.info("result : " + result);
 		} catch (Exception ex) {
 			logger.error("------------ Exception Occured  generateTOSCA() ----------- ", ex);
