418e8d27f95b7b080973edf626efa568e3b9c32e
Ashis Dutta Banik <ab00343130@techmahindra.com>
2018-02-21 21:41:15 -0600

    Change the manually created JSON format
    Remove the manually created JSON format and created a POJO class as 
    well using JACSON serialization of JSON format for 
    displaying the success and error message status as a JSON format.
    
    IssueId:ACUMOS-191
    Description : Set the check box to be stored in the cdump file
    
    Change-Id: Id63dfab1c8c6386656b2249d905b7f01946962f9
    Signed-off-by: Ashis Dutta Banik <ab00343130@techmahindra.com>

---
+++ ds-compositionengine/src/main/java/org/acumos/designstudio/ce/vo/SuccessErrorMessage.java
@@ -0,0 +1,40 @@
+package org.acumos.designstudio.ce.vo;
+
+import java.io.Serializable;
+
+public class SuccessErrorMessage implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -552558223759520244L;
+    private String success = "";
+    private String errorMessage = "";
+	
+	/**
+	 * @return the success
+	 */
+	public String getSuccess() {
+		return success;
+	}
+	/**
+	 * @param success the success to set
+	 */
+	public void setSuccess(String success) {
+		this.success = success;
+	}
+	/**
+	 * @return the errorMessage
+	 */
+	public String getErrorMessage() {
+		return errorMessage;
+	}
+	/**
+	 * @param errorMessage the errorMessage to set
+	 */
+	public void setErrorMessage(String errorMessage) {
+		this.errorMessage = errorMessage;
+	}
+    
+    
+}
