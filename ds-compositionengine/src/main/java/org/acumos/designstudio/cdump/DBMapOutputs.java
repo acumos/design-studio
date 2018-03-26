99ee8f32d3c82507d6aa635d3e16bd10eeb66763
Ashis Dutta Banik <ab00343130@techmahindra.com>
2018-03-26 15:41:11 -0500

    Add data broker related all POJO classes
    Change-Id: Ib5a2e04aeb8dec61c37063141b1643947bbc416a
    Signed-off-by: Ashis Dutta Banik <ab00343130@techmahindra.com>

---
+++ ds-compositionengine/src/main/java/org/acumos/designstudio/cdump/DBMapOutputs.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package org.acumos.designstudio.cdump;
+
+import java.io.Serializable;
+
+
+public class DBMapOutputs implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3848575029787965414L;
+	
+	private OutputField output_field;
+
+	/**
+	 * @return the output_field
+	 */
+	public OutputField getOutput_field() {
+		return output_field;
+	}
+
+	/**
+	 * @param output_field the output_field to set
+	 */
+	public void setOutput_field(OutputField output_field) {
+		this.output_field = output_field;
+	}
+	
+	
+	
+
+}
