9fdf1adb17a777ec3020036c6105bd94e6b484bf
Ashis Dutta Banik <ab00343130@techmahindra.com>
2018-03-27 15:32:19 -0500

    Add POJO classes to create JSONFormat of DB
    As per Data broker requirnment Source table data, Target table data
    and mapping related informations between Source and target
    tables are stored in CDUMP file.
    
    IssueId:ACUMOS-467,ACUMOS-474,ACUMOS-496,ACUMOS-503,ACUMOS-504,
    ACUMOS-505,ACUMOS-506
    Description : All the user stories are inter related each other
    
    Change-Id: I77cf2c3244afc2d22e399d62ecb971bcd0af0bf4
    Signed-off-by: Ashis Dutta Banik <ab00343130@techmahindra.com>

--- ds-compositionengine/src/main/java/org/acumos/designstudio/cdump/DataBrokerMap.java
+++ ds-compositionengine/src/main/java/org/acumos/designstudio/cdump/DataBrokerMap.java
@@ -17,17 +17,66 @@
  * limitations under the License.
  * ===============LICENSE_END=========================================================
  */
-
 package org.acumos.designstudio.cdump;
 
 import java.io.Serializable;
+
+/**
+ * This POKO DataBroker class defines different type of data broker types attribute,
+ * SQL script info attribute,field separator of CSV file and input and output fields mapping of CSV file as well 
+ * PROTObuf file input and output fields info.
+ */
 
 public class DataBrokerMap implements Serializable{
 
 	private static final long serialVersionUID = -4853419224669616315L;
 	
+	private String data_broker_type;
+	private DBMapInput[] map_inputs;
+	private DBMapOutput[] map_outputs;
 	private String script;
+	private String target_system_url;
+	private String local_system_data_file_path;
+	private String first_row;
+	private String csv_file_field_separator;
 	
+	/**
+	 * @return the data_broker_type
+	 */
+	public String getData_broker_type() {
+		return data_broker_type;
+	}
+	/**
+	 * @param data_broker_type the data_broker_type to set
+	 */
+	public void setData_broker_type(String data_broker_type) {
+		this.data_broker_type = data_broker_type;
+	}
+	
+	/**
+	 * @return the map_inputs
+	 */
+	public DBMapInput[] getMap_inputs() {
+		return map_inputs;
+	}
+	/**
+	 * @param map_inputs the map_inputs to set
+	 */
+	public void setMap_inputs(DBMapInput[] map_inputs) {
+		this.map_inputs = map_inputs;
+	}
+	/**
+	 * @return the map_outputs
+	 */
+	public DBMapOutput[] getMap_outputs() {
+		return map_outputs;
+	}
+	/**
+	 * @param map_outputs the map_outputs to set
+	 */
+	public void setMap_outputs(DBMapOutput[] map_outputs) {
+		this.map_outputs = map_outputs;
+	}
 	/**
 	 * @return the script
 	 */
@@ -40,4 +89,55 @@ public class DataBrokerMap implements Serializable{
 	public void setScript(String script) {
 		this.script = script;
 	}
+	/**
+	 * @return the target_system_url
+	 */
+	public String getTarget_system_url() {
+		return target_system_url;
+	}
+	/**
+	 * @param target_system_url the target_system_url to set
+	 */
+	public void setTarget_system_url(String target_system_url) {
+		this.target_system_url = target_system_url;
+	}
+	/**
+	 * @return the local_system_data_file_path
+	 */
+	public String getLocal_system_data_file_path() {
+		return local_system_data_file_path;
+	}
+	/**
+	 * @param local_system_data_file_path the local_system_data_file_path to set
+	 */
+	public void setLocal_system_data_file_path(String local_system_data_file_path) {
+		this.local_system_data_file_path = local_system_data_file_path;
+	}
+	/**
+	 * @return the first_row
+	 */
+	public String getFirst_row() {
+		return first_row;
+	}
+	/**
+	 * @param first_row the first_row to set
+	 */
+	public void setFirst_row(String first_row) {
+		this.first_row = first_row;
+	}
+	/**
+	 * @return the csv_file_field_separator
+	 */
+	public String getCsv_file_field_separator() {
+		return csv_file_field_separator;
+	}
+	/**
+	 * @param csv_file_field_separator the csv_file_field_separator to set
+	 */
+	public void setCsv_file_field_separator(String csv_file_field_separator) {
+		this.csv_file_field_separator = csv_file_field_separator;
+	}
+	
+	
+	
 }
