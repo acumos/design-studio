5e954591dc2734e7498b114e0f16292907fdd881
Ashis Dutta Banik <ab00343130@techmahindra.com>
2018-03-27 15:32:19 -0500

    Add POJO classes to create JSONFormat of DB
    Change-Id: I77cf2c3244afc2d22e399d62ecb971bcd0af0bf4
    Signed-off-by: Ashis Dutta Banik <ab00343130@techmahindra.com>

---
+++ ds-compositionengine/src/main/java/org/acumos/designstudio/cdump/DBMapOutput.java
@@ -0,0 +1,51 @@
+/*-
+ * ===============LICENSE_START=======================================================
+ * Acumos
+ * ===================================================================================
+ * Copyright (C) 2017 AT&T Intellectual Property & Tech Mahindra. All rights reserved.
+ * ===================================================================================
+ * This Acumos software file is distributed by AT&T and Tech Mahindra
+ * under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *  
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ * This file is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ===============LICENSE_END=========================================================
+ */
+package org.acumos.designstudio.cdump;
+
+import java.io.Serializable;
+/**
+ * This DataBroker POJO class defines output field attribute to construct output field json format 
+ * for CDUMP file.
+ */
+
+public class DBMapOutput implements Serializable {
+
+	private static final long serialVersionUID = -3848575029787965414L;
+	
+	private DBOutputField output_field;
+
+	/**
+	 * @return the output_field
+	 */
+	public DBOutputField getOutput_field() {
+		return output_field;
+	}
+
+	/**
+	 * @param output_field the output_field to set
+	 */
+	public void setOutput_field(DBOutputField output_field) {
+		this.output_field = output_field;
+	}
+	
+	
+	
+
+}
