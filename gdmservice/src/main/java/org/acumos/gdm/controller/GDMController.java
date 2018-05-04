2bc81000ea895a47a1668384976fe15e26861afd
VaibhavShirsat <vs00485966@techmahindra.com>
2018-05-04 18:04:46 +0530

    Committed GDM and CSVDB
    Issue-Id : ACUMOS-800
    Description : Construct CSVDatabroker as DS tool
    
    Change-Id: Ic550a9ca720781aebf1260a9a76684955e4e65b2
    Signed-off-by: VaibhavShirsat <vs00485966@techmahindra.com>

---
+++ gdmservice/src/main/java/org/acumos/gdm/controller/GDMController.java
@@ -0,0 +1,75 @@
+/*-
+ * ===============LICENSE_START=======================================================
+ * Acumos
+ * ===================================================================================
+ * Copyright (C) 2017 - 2018 AT&T Intellectual Property & Tech Mahindra. All rights reserved.
+ * ===================================================================================
+ * This Acumos software file is distributed by AT&T and Tech Mahindra
+ * under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *  
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ * This file is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ===============LICENSE_END=========================================================
+ */
+
+package org.acumos.gdm.controller;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.core.MediaType;
+
+import org.acumos.gdm.service.IGDMService;
+import org.acumos.gdm.util.EELFLoggerDelegator;
+import org.acumos.gdm.util.Properties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import io.swagger.annotations.ApiOperation;
+
+@RestController
+@RequestMapping(value = "/")
+public class GDMController {
+	private final EELFLoggerDelegator logger = EELFLoggerDelegator.getLogger(GDMController.class);
+	
+	@Autowired
+	Properties props;
+	
+	@Autowired
+	@Qualifier("GDMServiceImpl")
+	IGDMService gdmService;
+	
+	@ApiOperation(value = "transforms the protbuf format input data to the output and return outputdata in protobuf format")
+	@RequestMapping(value = "/mapData", method = RequestMethod.POST)
+	public byte[] mapData(@RequestBody byte[] inputData){
+		byte[] result = null;
+		logger.info("mapData : Begin");
+		InputStream inputStream = new ByteArrayInputStream(inputData);
+		OutputStream outputStream = gdmService.mapData(inputStream);
+		if(null == outputStream){
+			result = new byte[0];
+		} else {
+			result = ((ByteArrayOutputStream) outputStream).toByteArray();
+		}
+		
+		logger.info("mapData : End");
+		return result;
+	}
+
+}
