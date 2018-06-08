951e7a4948713e12e0e8bfc94f9fc8e2055a8862
Lott, Christopher (cl778h) <cl778h@att.com>
2018-06-08 10:28:46 -0400

    Add tiny models for demonstrating split/join
    Issue-ID: ACUMOS-1039
    Change-Id: Ic079f7f5e5606c4ec4e2032d33bb87db51402e90
    Signed-off-by: Lott, Christopher (cl778h) <cl778h@att.com>

---
+++ test-models/python/model_predict.py
@@ -0,0 +1,47 @@
+#! python
+# -*- coding: utf-8 -*-
+# ===============LICENSE_START=======================================================
+# Acumos Apache-2.0
+# ===================================================================================
+# Copyright (C) 2018 AT&T Intellectual Property & Tech Mahindra. All rights reserved.
+# ===================================================================================
+# This Acumos software file is distributed by AT&T and Tech Mahindra
+# under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# This file is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ===============LICENSE_END=========================================================
+"""
+Defines a pass-thru function as an Acumos model aka solution.
+Can use a session to on-board this to an Acumos instance.
+"""
+
+from demo_types import ComputeResultList
+from acumos.modeling import Model
+from acumos.session import AcumosSession
+
+def predict(l : ComputeResultList) -> ComputeResultList:
+    '''Passes thru a list of named tuple, each with one numeric value and one string value.'''
+    return l
+
+model = Model(op=predict)
+
+# Serialize the model for web on-boarding
+session = AcumosSession()
+basename = __file__.split('/')[-1] + '_dump'
+print('Using session to dump model to subdir {}'.format(basename))
+session.dump(model, basename, '.')
+
+# Example code
+
+# On-board the model directly
+#host='acumos-host-name.org'
+#session=AcumosSession(push_api='https://{}/onboarding-app/v2/models'.format(host), auth_api='https://{}/onboarding-app/v2/models/auth'.format(host))
+#print('on-boarding model to host {}'.format(host))
+#session.push(model, 'my-model')
