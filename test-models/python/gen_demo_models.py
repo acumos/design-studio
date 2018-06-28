8e172be3a13dc13fc783694c10bab99b4c5abb27
Lott, Christopher (cl778h) <cl778h@att.com>
2018-06-28 14:12:21 -0400

    Define additional demo models.
    Change-Id: I6bdf243c0bc90e2e297e7d1cd620030f16a69fc0
    Signed-off-by: Lott, Christopher (cl778h) <cl778h@att.com>

--- test-models/python/gen_demo_models.py
+++ test-models/python/gen_demo_models.py
@@ -25,6 +25,17 @@ import os, shutil
 from acumos.modeling import List, Model, NamedTuple
 from acumos.session import AcumosSession
 
+# Named Tuples
+
+class ConcatenateInput(NamedTuple):
+    '''Type representing an input to a string operation'''
+    s: str
+    d: float
+
+class ConcatenateOutput(NamedTuple):
+    '''Type representing an output from a string operation'''
+    joined: str
+
 class ComputeInput(NamedTuple):
     '''Type representing an input to a computation'''
     f1: float
@@ -40,6 +51,18 @@ class ComputeResultList(NamedTuple):
     '''Type representing a list of computation results'''
     l: List[ComputeResult]
 
+class ManipulateMessage(NamedTuple):
+    '''Type representing an input or output in a value manipulation'''
+    difference: float 
+    product: float
+    average: float
+
+class SquareMessage(NamedTuple):
+    '''Type representing an input or output in a computation'''
+    d: float
+
+# Methods
+
 def add(i: ComputeInput) -> ComputeResult:
     '''Returns a named tuple with the sum of the numeric arguments, and a revised string argument.'''
     return ComputeResult(i.f1 + i.f2, i.s + ' sum')
@@ -48,35 +71,113 @@ def average(i: ComputeInput) -> ComputeResult:
     '''Returns a named tuple with the mean of the numeric arguments, and a revised string argument.'''
     return ComputeResult((i.f1 + i.f2)/2.0, i.s + ' mean')
 
+def concatenate(i: ConcatenateInput) -> ConcatenateOutput:
+    '''Stringify a double and concatenate it with the another string input'''
+    return ConcatenateOutput(i.s + str(i.d))
+
 def classify(dci: ComputeInput) -> ComputeInput:
     '''Passes thru a named tuple with two numeric values and one string value.'''
     return dci
+
+def ingest(i: ComputeInput) -> ComputeInput:
+    '''Returns its input.'''
+    return i
+
+def manipulate(i: ManipulateMessage) -> ManipulateMessage:
+    '''Returns its input.'''
+    return i
 
 def multiply(i: ComputeInput) -> ComputeResult:
     '''Returns a named tuple with the product of the numeric arguments, and a revised string argument.'''
     return ComputeResult(i.f1 * i.f2, i.s + ' product')
 
+def output(i: ManipulateMessage) -> ManipulateMessage:
+    '''Returns its input.'''
+    return i
+
 def predict(l : ComputeResultList) -> ComputeResultList:
     '''Passes thru a list of named tuple, each with one numeric value and one string value.'''
     return l
+
+def square(i: SquareMessage) -> SquareMessage:
+    '''Returns a named tuple with the square of the numeric argument.'''
+    return SquareMessage(i.d * i.d)
 
 def subtract(i: ComputeInput) -> ComputeResult:
     '''Returns a named tuple with the difference of the numeric arguments, and a revised string argument.'''
     return ComputeResult(i.f1 - i.f2, i.s + ' difference')
 
-#session=AcumosSession()
+# Test the methods
+
+print('Test add')
+ci = ComputeInput(1.0, 2.0, "string")
+res = add(ci)
+assert(res.f >= 3.0)
+
+print('Test average')
+ci = ComputeInput(1.0, 2.0, "string")
+res = average(ci)
+assert(res.f > 1 and res.f < 2)
+
+print('Test concatenate')
+ci = ConcatenateInput('str', 1.0)
+res = concatenate(ci)
+assert('str' in res.joined and '1.0' in res.joined)
+
+print('Test classify')
+ci = ComputeInput(1.0, 2.0, "string")
+res = classify(ci)
+assert(res.f1 == ci.f1 and res.f2 == ci.f2 and res.s == ci.s)
+
+print('Test ingest')
+ci = ComputeInput(1.0, 2.0, "string")
+res = ingest(ci)
+assert(res.f1 == ci.f1 and res.f2 == ci.f2 and res.s == ci.s)
+
+print('Test manipulate')
+ci = ManipulateMessage(1.0, 2.0, 3.0)
+res = manipulate(ci)
+assert(res.difference == ci.difference and res.product == ci.product and res.average == ci.average)
+
+print('Test multiply')
+ci = ComputeInput(1.0, 2.0, "string")
+res = multiply(ci)
+assert(res.f >= 2.0)
+
+print('Test output')
+ci = ManipulateMessage(1.0, 2.0, 3.0)
+res = output(ci)
+assert(res.difference == ci.difference and res.product == ci.product and res.average == ci.average)
+
+print('Test predict')
+cr = ComputeResult(1.0, "str")
+ci = ComputeResultList( [ cr ] )
+res = predict(ci)
+assert(res == ci)
+
+print('Test square')
+ci = SquareMessage(2.0)
+res = square(ci)
+assert(res.d >= ci.d * ci.d)
+
+print('Test subtract')
+ci = ComputeInput(1.0, 2.0, "string")
+res = subtract(ci)
+assert(res.f < ci.f1 and res.f < ci.f2)
+
+# Dump and on-board the methods as models
 # This relies on the user entering a password on the command line
 push='http://cognita-dev1-vm01-core.eastus.cloudapp.azure.com:8090/onboarding-app/v2/models'
 auth='http://cognita-dev1-vm01-core.eastus.cloudapp.azure.com:8090/onboarding-app/v2/auth'
 session=AcumosSession(push_api=push, auth_api=auth)
 
-for f in add, average, classify, multiply, predict, subtract:
+for f in add, average, concatenate, classify, ingest, manipulate, multiply, output, predict, square, subtract:
     d = { f.__name__: f }
     model = Model(**d)
     subdir = 'dump_' + f.__name__
     # if the dump dir exists, assume it was pushed also
     if os.path.isdir(subdir):
-        print('Found dump dir, skipping {}'.format(f.__name__))
+        print('Found dump of model {}, skipping'.format(f.__name__))
     else:
         print('Dumping model {}'.format(f.__name__))
         session.dump(model, subdir, '.')
